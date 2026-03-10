ffb89e7f206c55ccf7aa9876bb9050d136917998
Kunal Ugale <kunalugale4060@gmail.com>
2026-03-10 11:04:31 +0530

    working

--- Array Basic/BASIC.java
+++ Array Basic/BASIC.java
@@ -1,12 +1,11 @@
-class BASIC{
-    public static void main(String[] args){
-        String arr = new arr[4];
+public class BASIC {
+    public static void main(String[] args) {
+        String[] arr = new String[4];
         arr[0] = "kunal";
         arr[1] = "shubham";
         arr[2] = "kunal";
         arr[3] = "kunal";
         System.out.println(arr[1]);
-
 
     }
 }
