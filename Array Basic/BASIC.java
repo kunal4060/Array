5a48ee50b182eb8a44daeda1eed1e7729642e2c9
Kunal Ugale <kunalugale4060@gmail.com>
2026-03-10 11:22:57 +0530

    understand of array

--- Array Basic/BASIC.java
+++ Array Basic/BASIC.java
@@ -1,11 +1,31 @@
+import static javax.swing.Action.NAME;
+
 public class BASIC {
     public static void main(String[] args) {
-        String[] arr = new String[4];
-        arr[0] = "kunal";
-        arr[1] = "shubham";
-        arr[2] = "kunal";
-        arr[3] = "kunal";
-        System.out.println(arr[1]);
+//        String[] arr = new String[4];
+//        arr[0] = "kunal";
+//        arr[1] = "shubham";
+//        arr[2] = "kunal";
+//        arr[3] = "kunal";
+//        System.out.println(arr[1]);
+        student[] s = new student[10];
+        s[0] = new student("kunal", "fdfs", 54);
+
+
 
     }
+}
+class student {
+    String NAME;
+    String ID;
+    int RollNo;
+
+    student(String NAME, String ID, int RollNo) {
+        this.NAME = NAME;
+        this.ID = ID;
+        this.RollNo = RollNo;
+
+
+    }
+
 }
